1916702107e71391b4cc4996bf12ccf796283845
lcy <lcy19961018@163.com>
2018-03-30 19:33:15 +0800

    publish to the internet.

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm75e(amokq7s9ll3j+j60ec)rn3m71@j(uju4f5$_6pygqxgh%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
